6e1b3497fa774d951171384ad1bc7b37102f5f8a
chaimass6 <chaitu.6914@gmail.com>
2024-12-28 18:07:34 +0530

    ok

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fetchResult =  async ()=>{
 
-    let url = `http://api.weatherapi.com/v1/current.json?key=b28d3f9d7f6d42dfa7f180626242212&q=Vijayawada&aqi=no`
+    let url = `http://api.weatherapi.com/v1/current.json?key=b28d3f9d7f6d42dfa7f180626242212&q=${}&aqi=no`
 
     const res  = await fetch(url)
     const data = await res.json()
